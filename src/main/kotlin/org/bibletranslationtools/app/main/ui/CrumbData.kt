29181c718aa3fc82947f403d6f37b0ab92e3f4d5
mxaln <murmax82@gmail.com>
2021-03-23 15:48:31 -0400

    Removed unused code

--- src/main/kotlin/org/bibletranslationtools/app/main/ui/CrumbData.kt
+++
@@ -1,5 +0,0 @@
-package org.bibletranslationtools.app.main.ui
-
-import org.kordamp.ikonli.javafx.FontIcon
-
-class CrumbData(val icon: FontIcon, val title: String)
--- src/main/kotlin/org/bibletranslationtools/app/main/ui/RootView.kt
+++ src/main/kotlin/org/bibletranslationtools/app/main/ui/RootView.kt
@@ -1,12 +1,5 @@
 package org.bibletranslationtools.app.main.ui
 
-import com.dukescript.layouts.jfxflexbox.FlexBoxPane
-import javafx.collections.ObservableList
-import javafx.geometry.Insets
-import javafx.geometry.Pos
-import javafx.scene.control.Label
-import javafx.scene.paint.Color
-import javafx.scene.paint.Paint
 import tornadofx.*
 
 class RootView : View() {
@@ -27,33 +20,5 @@ class RootView : View() {
 
     override fun onDock() {
         workspace.dock<HomeView>()
-    }
-
-    private fun makeLabel(
-        txt: String = "Label",
-        width: Double = 100.0,
-        height: Double = 100.0,
-        grow: Float = 1.0f,
-        bg: Paint = Color.WHITE
-    ): Label {
-
-        return label {
-            text = txt
-
-            prefWidth = width
-            maxWidth = Double.MAX_VALUE
-
-            prefHeight = height
-            maxHeight = Double.MAX_VALUE
-
-            alignment = Pos.CENTER
-
-            FlexBoxPane.setGrow(this, grow)
-            FlexBoxPane.setMargin(this, Insets(5.0))
-
-            style {
-                backgroundColor += bg
-            }
-        }
     }
 }
